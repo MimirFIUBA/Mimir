e92bc12d9d47787a316375a247c74eb40d4be3af
fede-longhi <federico.rlonghi@gmail.com>
2024-07-04 12:41:31 -0300

    create publisher and subscriber

--- src/main.go
+++ src/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
     mqtt "github.com/eclipse/paho.mqtt.golang"
+    "time"
 )
 
 var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
@@ -31,5 +32,27 @@ func main() {
     client := mqtt.NewClient(opts)
     if token := client.Connect(); token.Wait() && token.Error() != nil {
         panic(token.Error())
-  }
+    }
+
+    sub(client)
+    publish(client)
+
+    client.Disconnect(250)
+}
+
+func publish(client mqtt.Client) {
+    num := 10
+    for i := 0; i < num; i++ {
+        text := fmt.Sprintf("Message %d", i)
+        token := client.Publish("topic/test", 0, false, text)
+        token.Wait()
+        time.Sleep(time.Second)
+    }
+}
+
+func sub(client mqtt.Client) {
+    topic := "topic/test"
+    token := client.Subscribe(topic, 1, nil)
+    token.Wait()
+  	fmt.Printf("Subscribed to topic: %s", topic)
 }
