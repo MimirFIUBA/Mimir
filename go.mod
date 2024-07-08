67458326ab5c2099f4d9d9c5dadc6ad2b37f37a3
fede-longhi <federico.rlonghi@gmail.com>
2024-07-08 15:29:01 -0300

    sensor values with multiple types

--- go.mod
+++ go.mod
@@ -1,8 +1,14 @@
 module mimir
 
-go 1.16
+go 1.22
 
 require (
-	github.com/eclipse/paho.mqtt.golang v1.4.3 // indirect
-	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/eclipse/paho.mqtt.golang v1.4.3
+	github.com/gorilla/mux v1.8.1
+)
+
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 )
--- src/mimir/sensor.go
+++ src/mimir/sensor.go
@@ -2,10 +2,10 @@ package mimir
 
 import (
 	"fmt"
+	"time"
 )
 
 var (
-	SensorChannel = make(chan Sensor, 1)
 	sensorManager = SensorManager{}
 )
 
@@ -13,7 +13,15 @@ type Sensor struct {
 	ID int `json:"id"`
 	Name string `json:"name"`
 	Description string `json:"description"`
+	Data []SensorReading `json:"data"`
 }
+
+type SensorReading struct {
+	Value SensorValue `json:"value"`
+	Time time.Time `json:"time"`
+}
+
+type SensorValue interface { }
 
 type SensorManager struct {
 	sensors []Sensor
@@ -34,5 +42,32 @@ func GetSensors() []Sensor {
 	return sensorManager.sensors
 }
 
-func Run () {
+func Run() {
+
+	// Load Config
+	sensor := Sensor{0, "fede", "this is a helpful sensor", nil}
+	
+	var value1 SensorValue
+	value1 = 1
+	data := SensorReading{Value: value1, Time: time.Now()}
+	sensor.Data = append(sensor.Data, data)
+	
+	var value2 SensorValue
+	value2 = 1.3
+	data2 := SensorReading{Value: value2, Time: time.Now()}
+	sensor.Data = append(sensor.Data, data2)
+	
+	var value3 SensorValue
+	value3 = true
+	data3 := SensorReading{Value: value3, Time: time.Now()}
+	sensor.Data = append(sensor.Data, data3)
+	
+	var valueString SensorValue
+	valueString = "too high"
+	data4 := SensorReading{Value: valueString, Time: time.Now()}
+	sensor.Data = append(sensor.Data, data4)
+
+	fmt.Printf("sensorData: %+v\n", len(sensor.Data))
+	CreateSensor(sensor)
+
 }
