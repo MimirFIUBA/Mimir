0a603b06acaa0573623129341e0d02a0c006be97
fede-longhi <federico.rlonghi@gmail.com>
2024-10-06 19:52:21 -0300

    connection to influx db

---
+++ db/db_test.go
@@ -0,0 +1,41 @@
+package db_test
+
+import (
+	"context"
+	"mimir/db"
+	"testing"
+
+	"github.com/influxdata/influxdb-client-go/v2/domain"
+	"github.com/joho/godotenv"
+)
+
+func Test_connectToInfluxDB(t *testing.T) {
+
+	//load environment variable from a file for test purposes
+	godotenv.Load("../db/test_influxdb.env")
+
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{
+			name:    "Successful connection to InfluxDB",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := db.ConnectToInfluxDB()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ConnectToInfluxDB() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			health, err := got.Health(context.Background())
+			if (err != nil) && health.Status == domain.HealthCheckStatusPass {
+				t.Errorf("connectToInfluxDB() error. database not healthy")
+				return
+			}
+			got.Close()
+		})
+	}
+}
