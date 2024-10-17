da40a8c8b63c98bef4054b9839ee1bd599c26ac2
Luis Albarracín <aalbarra@fi.uba.ar>
2024-10-17 16:19:21 -0300

    Add mongodb docker

--- db/mongodb/mongo.go
+++ db/mongodb/mongo.go
@@ -20,12 +20,19 @@ func Connect() (*mongo.Client, error) {
 
 	dbPassword := os.Getenv("MONGODB_PASSWORD")
 	if dbPassword == "" {
-		return nil, errors.New("INFLUXDB_URL must be set")
+		return nil, errors.New("MONGODB_PASSWORD must be set")
 	}
+
+	dbLocal := os.Getenv("MONGODB_LOCAL")
 
 	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	uri := fmt.Sprintf("mongodb+srv://%s:%s@mimir.razfo.mongodb.net/?retryWrites=true&w=majority&appName=Mimir", dbUsername, dbPassword)
+	var uri string
+	if dbLocal == "true" {
+		uri = fmt.Sprintf("mongodb://%s:%s@localhost:27017/?retryWrites=true&w=majority&appName=Mimir", dbUsername, dbPassword)
+	} else {
+		uri = fmt.Sprintf("mongodb+srv://%s:%s@mimir.razfo.mongodb.net/?retryWrites=true&w=majority&appName=Mimir", dbUsername, dbPassword)
+	}
 	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
 	// Create a new client and connect to the server
 	client, err := mongo.Connect(context.TODO(), opts)
