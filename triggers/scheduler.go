ffa86637140aa61419524b4bf0601a86077d655b
fede-longhi <federico.rlonghi@gmail.com>
2024-11-02 19:09:06 -0300

    scheduler

---
+++ triggers/scheduler.go
@@ -0,0 +1,42 @@
+package triggers
+
+import (
+	"time"
+
+	"github.com/go-co-op/gocron/v2"
+)
+
+type Scheduler struct {
+	gocronScheduler gocron.Scheduler
+}
+
+func NewScheduler() (*Scheduler, error) {
+	s, err := gocron.NewScheduler()
+	if err != nil {
+		return nil, err
+	}
+	return &Scheduler{s}, nil
+}
+
+func (s *Scheduler) ScheduleTrigger(cronExpresion string, trigger Trigger) {
+	s.gocronScheduler.NewJob(
+		gocron.CronJob(cronExpresion, true),
+		gocron.NewTask(
+			func() {
+				trigger.Update(Event{
+					SenderId:  "Scheduler",
+					Type:      SCHEDULER_ACTIVE,
+					Timestamp: time.Now(),
+				})
+			},
+		),
+	)
+}
+
+func (s *Scheduler) Start() {
+	s.gocronScheduler.Start()
+}
+
+func (s *Scheduler) Shutdown() {
+	s.gocronScheduler.Shutdown()
+}
